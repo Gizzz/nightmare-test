ca9e7d53d9ee21905baa3573723fcdf066d4b2ad
Gizzz <bulatkhusainov@yandex.ru>
2017-08-07 21:05:41 +0300

    add page loading test (login page)

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const Nightmare = require('nightmare')
 const assert = require('assert')
 
-describe('Load a Page', function() {
+describe('Public Pages', function() {
   // Recommended: 5s locally, 10s to remote server, 30s from airplane ¯\_(ツ)_/¯
   this.timeout('30s')
 
   let nightmare = null
   beforeEach(() => {
-    nightmare = new Nightmare()
+    nightmare = new Nightmare({ show: true })
   })
 
   describe('/ (Home Page)', () => {
@@ -16,6 +16,15 @@ describe('Load a Page', function() {
       nightmare.goto('https://gethoodie.com')
         .end()
         .then(function (result) { done() })
+        .catch(done)
+    })
+  })
+
+  describe('/auth (Login Page)', () => {
+    it('should load without error',  done => {
+      nightmare.goto('https://gethoodie.com/auth')
+        .end()
+        .then(result => { done() })
         .catch(done)
     })
   })
