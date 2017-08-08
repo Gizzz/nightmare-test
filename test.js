795bf01142ee3b200b0999b77bdb81f44154127d
Gizzz <bulatkhusainov@yandex.ru>
2017-08-08 20:10:30 +0300

    add test for testing main page load

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-describe("app", () => {
-  it("should not fail", () => {
-    expect(true).toBe(true);
+const oneHour = 1000 * 3600;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = oneHour;
+
+describe('app', () => {
+  // it('should not fail', () => {
+  //   expect(true).toBe(true);
+  // });
+
+  it('shold load main page', (done) => {
+    const Nightmare = require('nightmare');
+    const nightmare = new Nightmare({
+      show: true,
+      waitTimeout: oneHour,
+      width: 1480,
+      height: 800,
+    });
+
+    nightmare
+      .goto('https://stark-cliffs-10206.herokuapp.com/')
+      .end()
+      .then(done);
   });
 });
