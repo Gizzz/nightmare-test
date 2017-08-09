97e4cc88109051a31fc13a1b5ed1cea8fa38ac0e
Gizzz <bulatkhusainov@yandex.ru>
2017-08-09 16:12:54 +0300

    add async test

--- test.js
+++ test.js
@@ -1,12 +1,34 @@
 const oneHour = 1000 * 3600;
-jasmine.DEFAULT_TIMEOUT_INTERVAL = oneHour;
+// jasmine.DEFAULT_TIMEOUT_INTERVAL = oneHour;
+
+// function fetchData(callback) {
+//   setTimeout(function() {
+//     callback();
+//   }, 1000);
+// }
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      () => resolve('res'), 
+      1000,
+    );
+  });
+}
 
 describe('app', () => {
-  // it('should not fail', () => {
-  //   expect(true).toBe(true);
-  // });
+  it('testing async', async () => {
+    const result = await fetchData();
+    expect(result).toBe('res');
 
-  it('shold load main page', (done) => {
+    return expect(fetchData()).resolves.toBe('res');
+  });
+
+  xit('should not fail', () => {
+    expect(true).toBe(true);
+  });
+
+  xit('shold load main page', (done) => {
     const Nightmare = require('nightmare');
     const nightmare = new Nightmare({
       show: true,
