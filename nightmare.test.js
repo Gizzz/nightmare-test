dddf651d217e512eea94dc5ff326a771330a1da5
Gizzz <bulatkhusainov@yandex.ru>
2017-08-12 01:34:21 +0300

    remove test from nightmare

--- nightmare.test.js
+++ nightmare.test.js
@@ -9,7 +9,7 @@ describe('app', () => {
     const nightmare = new Nightmare({
       show: true,
       width: 1480,
-      height: 800,
+      height: 1000,
       waitTimeout: oneHour,
     });
 
@@ -27,7 +27,7 @@ describe('app', () => {
 
     const itemPagePath = await nightmare
       .goto(baseUrl + listPagePath)
-      // wait untill items loaded
+      // wait untill items loaded      
       .wait(1000)
       // click on first item in list
       .click('div#root > div.app:nth-child(1) > section.main:nth-child(2) > div.wrapper:nth-child(1) > span:nth-child(1) > div.content.list:nth-child(1) > ul.items:nth-child(3) > li:nth-child(1) > a:nth-child(1) > img:nth-child(1)')
@@ -41,8 +41,8 @@ describe('app', () => {
       .goto(baseUrl + itemPagePath)
       // click 'add to cart' button
       .click('div#root > div.app:nth-child(1) > section.main:nth-child(2) > div.wrapper:nth-child(1) > span:nth-child(1) > div.content.detail:nth-child(1) > div.row:nth-child(1) > div.col.text:nth-child(2) > form:nth-child(3) > div.add_to_cart-btn:nth-child(4) > button:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)')
-      // click 'view cart' in modal dialog
       .wait(1000)
+      // click 'view cart' in modal dialog
       .click('html > body > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(1) > div:nth-child(1) > span:nth-child(1)')
       .wait(1000)
       .evaluate(() => {
@@ -51,27 +51,5 @@ describe('app', () => {
       .end();
 
     expect(cartPagePath).toBe('/cart');
-  });
-
-  xit('should navigate to item page', async () => {
-    const Nightmare = require('nightmare');
-    const nightmare = new Nightmare({
-      show: true,
-      width: 1480,
-      height: 800,
-      waitTimeout: oneHour,
-    });
-
-    const listPagePath = '/list/mens_outerwear';
-
-    const itemPagePath = await nightmare
-      .goto(baseUrl + listPagePath)
-      .click('div#root > div.app:nth-child(1) > section.main:nth-child(2) > div.wrapper:nth-child(1) > span:nth-child(1) > div.content.list:nth-child(1) > ul.items:nth-child(3) > li:nth-child(1) > a:nth-child(1) > img:nth-child(1)')
-      .evaluate(() => {
-        return window.location.pathname;
-      })
-      .end();
-      
-    expect(itemPagePath).toBe('/detail/mens_outerwear/Men+s+Tech+Shell+Full-Zip');
   });
 });
