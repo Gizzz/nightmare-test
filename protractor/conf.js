12f556d1f031382cba007dd9e28e25a1fafcc665
Gizzz <bulatkhusainov@yandex.ru>
2017-08-11 22:46:04 +0300

    add protractor tests

---
+++ protractor/conf.js
@@ -0,0 +1,9 @@
+exports.config = {
+	directConnect: true,
+	capabilities: {
+    'browserName': 'chrome'
+  },
+	// framework: 'jasmine',
+  specs: ['todo-spec.js'],
+	// baseUrl: 'http://localhost:3000',
+};
